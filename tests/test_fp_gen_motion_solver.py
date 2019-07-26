4d3d519419be0d819fdc20d9692ec143c30b6329
Adam Lamson <adam.lamson@colorado.edu>
2019-07-25 21:21:34 -0600

    Finished inheritance testing FPGenMotionSolver

--- tests/test_fp_gen_motion_solver.py
+++ tests/test_fp_gen_motion_solver.py
@@ -2,6 +2,7 @@
 import pytest
 from foxlink.FP_gen_motion_solver import FPGenMotionSolver
 from foxlink.FP_gen_orient_solver import FPGenOrientSolver
+from foxlink.rod_motion_solver import RodMotionSolver
 from foxlink.solver import Solver
 
 """@package docstring
@@ -19,9 +20,18 @@ def fp_gen_motion_solver():
 
 def test_gen_motion_solver_inheritance():
     """!TODO: Docstring for test_gen_motion_solver.
-    @return: TODO
+    @return: void
 
     """
-    assert FPGenMotionSolver.Write is FPGenOrientSolver.Write
     assert FPGenMotionSolver.ParseParams is FPGenOrientSolver.ParseParams
-    # assert FPGenMotionSolver.Write is not Solver.Write
+    assert FPGenMotionSolver.makeSolutionGrid is Solver.makeSolutionGrid
+    assert FPGenMotionSolver.setInitialCondition is Solver.setInitialCondition
+    assert FPGenMotionSolver.Run is Solver.Run
+    assert FPGenMotionSolver.Step is Solver.Step
+    assert FPGenMotionSolver.makeDataframe is FPGenOrientSolver.makeDataframe
+    assert FPGenMotionSolver.calcForceMatrix is FPGenOrientSolver.calcForceMatrix
+    assert FPGenMotionSolver.calcTorqueMatrix is FPGenOrientSolver.calcTorqueMatrix
+    assert FPGenMotionSolver.calcSourceMatrix is FPGenOrientSolver.calcSourceMatrix
+    assert FPGenMotionSolver.Write is FPGenOrientSolver.Write
+    assert FPGenMotionSolver.Save is Solver.Save
+    assert FPGenMotionSolver.RodStep is RodMotionSolver.RodStep
