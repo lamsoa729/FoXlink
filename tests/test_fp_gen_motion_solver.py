11a7f5686e0ebd7460786e4be7f9478ff81629a4
Adam Lamson <adam.lamson@colorado.edu>
2019-07-25 21:32:34 -0600

    Added solver back into unit test

--- tests/test_fp_gen_motion_solver.py
+++ tests/test_fp_gen_motion_solver.py
@@ -3,6 +3,7 @@ import pytest
 from foxlink.FP_gen_motion_solver import FPGenMotionSolver
 from foxlink.FP_gen_orient_solver import FPGenOrientSolver
 from foxlink.rod_motion_solver import RodMotionSolver
+from foxlink.solver import Solver
 
 """@package docstring
 File: test_fp_gen_motion_solver.py
