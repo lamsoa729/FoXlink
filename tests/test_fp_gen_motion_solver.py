6cfb23338345281d7c7d63db99803eed70a5318f
Adam Lamson <adam.lamson@colorado.edu>
2019-07-25 21:28:13 -0600

    Commented out unnecessary fixture

--- tests/test_fp_gen_motion_solver.py
+++ tests/test_fp_gen_motion_solver.py
@@ -3,7 +3,6 @@ import pytest
 from foxlink.FP_gen_motion_solver import FPGenMotionSolver
 from foxlink.FP_gen_orient_solver import FPGenOrientSolver
 from foxlink.rod_motion_solver import RodMotionSolver
-from foxlink.solver import Solver
 
 """@package docstring
 File: test_fp_gen_motion_solver.py
@@ -13,9 +12,9 @@ Description:
 """
 
 
-@pytest.fixture()
-def fp_gen_motion_solver():
-    return FPGenMotionSolver()
+# @pytest.fixture()
+# def fp_gen_motion_solver():
+# return FPGenMotionSolver()
 
 
 def test_gen_motion_solver_inheritance():
