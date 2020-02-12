8d87f7316281d5874b10228ad46e73c14f2da3ef
Adam Lamson <adam.lamson@colorado.edu>
2020-02-12 12:06:07 -0700

    Created prototype functions necessary for wca forces

--- foxlink/rod_steric_forces.py
+++ foxlink/rod_steric_forces.py
@@ -25,12 +25,56 @@ def wca_force(dr, sigma, eps):
     r_inv = 1. / r_mag
     u_vec = dr * r_inv
     r_inv6 = r_inv**6
-
-    rcut = np.power(2.0, 1.0 / 6.0) * sigma
     sigma6 = sigma**6
 
-    fmag = 24. * eps * sigma6 * (r_inv6 * r_inv) * (2. * sigma6 * r_inv6 -
-                                                    1.) if r_mag < rcut else 0.
-    return fmag * u_vec
+    rcut = np.power(2.0, 1.0 / 6.0) * sigma
 
-# TODO: Torque function from WCA <11-02-20, ARL> #
+    # Impose force only if LJ potential is repulsive
+    f_mag = (24. * eps * sigma6 * r_inv6 * r_inv) * (
+        2. * sigma6 * r_inv6 - 1.) if r_mag < rcut else 0.
+    return f_mag * u_vec
+
+
+def find_sphero_min_dist(r_i, r_j, u_i, u_j, L_i, L_j):
+    """!Find the minimum distance between two spherocylinders (i,j) and the
+    points on the spherocylinders where that minimum distance occurs. Minimum
+    distance points are relative to the center of the spherocylinders.
+
+    @param r_i: Center of rod i
+    @param r_j: Center of rod j
+    @param u_i: Orientation vector of rod i
+    @param u_j: Orientation vector of rod j
+    @param L_i: Length of rod i
+    @param L_j: Length of rod j
+    @return: Minimum distance vector pointing from i to j,
+    point of minimum distance on rod i, point of minium distance rod j
+
+    """
+    min_vec_ij = 0
+    l_i = 0
+    l_j = 0
+
+    return min_vec_ij, l_i, l_j
+
+
+def calc_wca_force_torque(r_i, r_j, u_i, u_j, L_i, L_j, rod_diameter, eps):
+    """!Calculate and return the forces and torques on two spherocylinders (i,j).
+
+    @param r_i: TODO
+    @param r_j: TODO
+    @param u_i: TODO
+    @param u_j: TODO
+    @param L_i: TODO
+    @param L_j: TODO
+    @param rod_diameter: TODO
+    @param eps: TODO
+    @return: TODO
+
+    """
+    dr_ij, l_i, l_j = find_sphero_min_dist(r_i, r_j, u_i, u_j, L_i, L_j)
+
+    f_ij = wca_force(dr_ij, rod_diameter, eps)
+    tau_i = np.cross(l_i * u_i, -dr_ij)
+    tau_j = np.cross(l_j * u_j, dr_ij)
+
+    return f_ij, tau_i, tau_j
