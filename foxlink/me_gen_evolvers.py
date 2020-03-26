1f49abf5201ff9d420d2325aff7d6a579002c2cc
Adam Lamson <adam.lamson@colorado.edu>
2020-03-26 17:17:04 -0600

    Changed how me general evolvers implement their code.

--- foxlink/me_gen_evolvers.py
+++ foxlink/me_gen_evolvers.py
@@ -17,7 +17,7 @@ from .me_gen_odes import (du_dt_gen_2ord, dmu00_dt_gen, dmu10_dt_gen_2ord,
                           dmu11_dt_gen_2ord, dmu20_dt_gen_2ord)
 
 
-def prep_me_evolver_gen_2ord(sol, co, ks, ho, beta, L_i, L_j):
+def prep_me_evolver_gen_2ord(sol, params):
     """!Calculate necessary variables to evolve the solution.
 
     @param sol: TODO
@@ -35,6 +35,13 @@ def prep_me_evolver_gen_2ord(sol, co, ks, ho, beta, L_i, L_j):
     a_ij = np.dot(r_ij, u_i)
     a_ji = -1.0 * np.dot(r_ij, u_j)
     b = np.dot(u_i, u_j)
+
+    co = params['co']
+    ks = params['ks']
+    ho = params['ho']
+    beta = params['beta']
+
+    L_i, L_j = params['L1'], params['L2']
 
     # Convert solution into readable moments to use in derivatives
     (mu00, mu10, mu01, mu11, mu20, mu02) = sol[12:18].tolist()
@@ -58,14 +65,12 @@ def prep_me_evolver_gen_2ord(sol, co, ks, ho, beta, L_i, L_j):
                        lambda s_j: -.5 * L_j, lambda s_j: .5 * L_j,
                        args=[0, 2, rsqr, a_ij, a_ji, b, ks, ho, beta], epsrel=1e-5)[0]
     return (r_ij, u_i, u_j,  # Vector quantities
-            rsqr, a_ij, a_ji, b,  # Scalar geometric quantities
-            mu00, mu10, mu01, mu11, mu20, mu02,
-            q00, q10, q01, q11, q20, q02)
+            [rsqr, a_ij, a_ji, b],  # Scalar geometric quantities
+            [mu00, mu10, mu01, mu11, mu20, mu02],
+            [q00, q10, q01, q11, q20, q02])
 
 
-def me_evolver_gen_2ord(sol,
-                        gpara_i, gperp_i, grot_i, gpara_j, gperp_j, grot_j,
-                        vo, fs, ko, co, ks, ho, beta, L_i, L_j):
+def me_evolver_gen_2ord(sol, fric_coeff, params):
     """!Calculate all time derivatives necessary to solve the moment expansion
     evolution of the Fokker-Planck equation of zero rest length (gen) crosslinkers
     bound to moving rods. d<var> is the time derivative of corresponding variable
@@ -73,41 +78,35 @@ def me_evolver_gen_2ord(sol,
     @param sol: Current solution of ODE
     @param gpara_ij: Parallel drag coefficient of rod_i
     @param gperp_i: Perpendicular drag coefficient of rod_i
-    @param grot_i: Rotational drag coefficient of rod_i
-    @param gpara_ji: Parallel drag coefficient of rod_i
-    @param gperp_j: Perpendicular drag coefficient of rod_i
-    @param grot_j: Rotational drag coefficient of rod_i
-    @param vo: Velocity of motor when no force is applied
-    @param fs: Stall force of motor ends
-    @param ko: Turnover rate of motors
-    @param co: Effective concentration of motors in solution
-    @param ks: Motor spring constant
-    @param beta: 1/(Boltzmann's constant * Temperature)
-    @param L_i: Length of rod_i
-    @param L_j: Length of rod_j
     @return: Time-derivatives of all time varying quantities in a flattened
              array
     """
+    co = params['co']
+    ks = params['ks']
+    ho = params['ho']
+    ko = params['ko']
+    vo = params['vo']
+    fs = params['fs']
+    beta = params['beta']
     # Get variables needed to solve ODE
     (r_ij, u_i, u_j, rsqr, a_ij, a_ji, b,
      mu00, mu10, mu01, mu11, mu20, mu02,
-     q00, q10, q01, q11, q20, q02) = prep_me_evolver_gen_2ord(sol, co, ks, ho,
-                                                              beta, L_i, L_j)
+     q00, q10, q01, q11, q20, q02) = prep_me_evolver_gen_2ord(sol, params)
 
     # Get average force of crosslinkers on rod_j
     f_ij = avg_force_gen_2ord(r_ij, u_i, u_j, rsqr, a_ij, a_ji, b,
                               mu00, mu10, mu01, mu11, mu20, mu02,
                               ks, ho)
     # Evolution of rod positions
-    dr_i = dr_dt(-1. * f_ij, u_i, gpara_i, gperp_i)
-    dr_j = dr_dt(f_ij, u_j, gpara_j, gperp_j)
+    dr_i = dr_dt(-1. * f_ij, u_i, fric_coeff[0], fric_coeff[1])
+    dr_j = dr_dt(f_ij, u_j, fric_coeff[3], fric_coeff[4])
     # Evolution of orientation vectors
     du_i = du_dt_gen_2ord(r_ij, u_i, u_j, rsqr, a_ij, a_ji, b,
                           mu10, mu11, mu20,
-                          ks, ho, grot_i)
+                          ks, ho, fric_coeff[2])
     du_j = du_dt_gen_2ord(-1. * r_ij, u_j, u_i, rsqr, a_ji, a_ij, b,  # ij-ji
                           mu01, mu11, mu02,  # kl->lk
-                          ks, ho, grot_j)
+                          ks, ho, fric_coeff[5])
     # Evolution of zeroth moment
     dmu00 = dmu00_dt_gen(mu00, ko, q00)
     # Evoultion of first moments
@@ -138,9 +137,7 @@ def me_evolver_gen_2ord(sol,
     return dsol
 
 
-def me_evolver_gen_orient_2ord(sol,
-                               gpara_i, gperp_i, grot_i, gpara_j, gperp_j, grot_j,
-                               vo, fs, ko, co, ks, ho, beta, L_i, L_j):
+def me_evolver_gen_orient_2ord(sol, fric_coeff, params):
     """!Calculate all time derivatives necessary to solve the moment expansion
     evolution of the Fokker-Planck equation of zero rest length (gen) crosslinkers
     bound to moving rods. d<var> is the time derivative of corresponding variable
@@ -148,34 +145,28 @@ def me_evolver_gen_orient_2ord(sol,
     @param sol: Current solution of ODE
     @param gpara_ij: Parallel drag coefficient of rod_i
     @param gperp_i: Perpendicular drag coefficient of rod_i
-    @param grot_i: Rotational drag coefficient of rod_i
-    @param gpara_ji: Parallel drag coefficient of rod_i
-    @param gperp_j: Perpendicular drag coefficient of rod_i
-    @param grot_j: Rotational drag coefficient of rod_i
-    @param vo: Velocity of motor when no force is applied
-    @param fs: Stall force of motor ends
-    @param ko: Turnover rate of motors
-    @param co: Effective concentration of motors in solution
-    @param ks: Motor spring constant
-    @param beta: 1/(Boltzmann's constant * Temperature)
-    @param L_i: Length of rod_i
-    @param L_j: Length of rod_j
     @return: Time-derivatives of all time varying quantities in a flattened
              array
     """
+    co = params['co']
+    ks = params['ks']
+    ho = params['ho']
+    ko = params['ko']
+    vo = params['vo']
+    fs = params['fs']
+    beta = params['beta']
     # Get variables needed to solve ODE
     (r_ij, u_i, u_j, rsqr, a_ij, a_ji, b,
      mu00, mu10, mu01, mu11, mu20, mu02,
-     q00, q10, q01, q11, q20, q02) = prep_me_evolver_gen_2ord(sol, co, ks, ho,
-                                                              beta, L_i, L_j)
+     q00, q10, q01, q11, q20, q02) = prep_me_evolver_gen_2ord(sol, params)
 
     # Get average force of crosslinkers on rod_j
     f_ij = avg_force_gen_2ord(r_ij, u_i, u_j, rsqr, a_ij, a_ji, b,
                               mu00, mu10, mu01, mu11, mu20, mu02,
                               ks, ho)
     # Evolution of rod positions
-    dr_i = dr_dt(-1. * f_ij, u_i, gpara_i, gperp_i)
-    dr_j = dr_dt(f_ij, u_j, gpara_j, gperp_j)
+    dr_i = dr_dt(-1. * f_ij, u_i, fric_coeff[0], fric_coeff[1])
+    dr_j = dr_dt(f_ij, u_j, fric_coeff[3], fric_coeff[4])
     # Orientations are not updated
     du_arr = np.zeros(6)
     # Evolution of zeroth moment
@@ -213,13 +204,3 @@ def me_evolver_gen_orient_2ord(sol,
 # me_evolver_gen_stat_2ord
 # me_evolver_gen_ang_2ord
 # me_evolver_gen_orient_2ord
-# @param r_i: Center of mass postion of rod_i
-# @param r_j: Center of mass position of rod_j
-# @param u_i: Orientation unit vector of rod_i
-# @param u_j: Orientation unit vector of rod_j
-# @param mu00: Zeroth motor moment
-# @param mu10: First motor moment of s_i
-# @param mu01: First motor moment of s_j
-# @param mu11: Second motor moment of s_i and s_j
-# @param mu20: Second motor moment of s_i
-# @param mu02: Second motor moment of s_j
